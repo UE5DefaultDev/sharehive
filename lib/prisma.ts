88f48d5b639cf7be657ae00f9a45db08aec15688
UE5DefaultDev <lorintraut@gmail.com>
2025-12-07 14:19:50 +0100

    #

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,15 +1,17 @@
-import { PrismaClient } from "@prisma/client";
+import { PrismaClient } from "@prisma/client"
+import { PrismaPg } from '@prisma/adapter-pg'
+import { Pool } from 'pg'
 
-const prismaClientSingleton = () => {
-  return new PrismaClient();
-};
+declare global {
+  var prisma: PrismaClient | undefined
+}
 
-declare const globalThis: {
-  prismaGlobal: ReturnType<typeof prismaClientSingleton>;
-} & typeof global;
+const connectionString = `${process.env.DATABASE_URL}`
+const pool = new Pool({ connectionString })
+const adapter = new PrismaPg(pool)
 
-const prisma = globalThis.prismaGlobal ?? prismaClientSingleton();
+const prisma = global.prisma || new PrismaClient({ adapter });
 
-export default prisma;
+if (process.env.NODE_ENV !== "production") global.prisma = prisma;
 
-if (process.env.NODE_ENV !== "production") globalThis.prismaGlobal = prisma;
+export default prisma
